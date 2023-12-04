3955ae768d9e3de0d460af42d330533d3924f497
kavvata <gabriel.kawata@gmail.com>
2023-12-04 15:10:46 -0300

    opcao listar

--- app.dart
+++ app.dart
@@ -3,10 +3,11 @@ import 'dart:io';
 void main() {
   const Map<int, Function> acoes = {
     0: sair,
-    1: cadastrar,
-    2: buscar,
-    3: atualizar,
-    4: remover
+    1: listar,
+    2: cadastrar,
+    3: buscar,
+    4: atualizar,
+    5: remover
   };
 
   int opcaoUsuario = -1;
@@ -25,8 +26,7 @@ void main() {
     }
 
     // ! -> assume acoes[i] como nunca nulo
-    // seguro, pois opcaoUsuario é checada antes de chegar
-    // nessa linha
+    // seguro, opcaoUsuario é checado antes de chegar nessa linha
     acoes[opcaoUsuario]!.call();
   }
 }
@@ -34,10 +34,11 @@ void main() {
 void mostraMenu() {
   String conteudo = "";
 
-  conteudo += "1 - Cadastrar nova pessoa\n";
-  conteudo += "2 - Buscar Pessoa\n";
-  conteudo += "3 - Atualizar Pessoa\n";
-  conteudo += "4 - Remover Pessoa\n";
+  conteudo += "1 - Listar pessoas\n";
+  conteudo += "2 - Cadastrar nova pessoa\n";
+  conteudo += "3 - Buscar pessoa\n";
+  conteudo += "4 - Atualizar pessoa\n";
+  conteudo += "5 - Remover pessoa\n";
   conteudo += "0 - Sair\n";
 
   stdout.write(conteudo + "> ");
@@ -47,18 +48,27 @@ void sair() {
   exit(0);
 }
 
+void listar() {
+  // TODO
+  print("listar");
+}
+
 void cadastrar() {
+  // TODO
   print("cadastrar");
 }
 
 void buscar() {
+  // TODO
   print("buscar");
 }
 
 void atualizar() {
+  // TODO
   print("atualizar");
 }
 
 void remover() {
+  // TODO
   print("remover");
 }
