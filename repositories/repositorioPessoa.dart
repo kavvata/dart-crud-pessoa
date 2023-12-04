835262d1e3ddae83c96f9125f267198989dd1baf
kavvata <gabriel.kawata@gmail.com>
2023-12-04 15:09:05 -0300

    repositorio

---
+++ repositories/repositorioPessoa.dart
@@ -0,0 +1,26 @@
+import '../daos/pessoaDAO.dart';
+import '../entities/Pessoa.dart';
+
+class RepositorioPessoa {
+  PessoaDAO _dao;
+
+  RepositorioPessoa(this._dao);
+
+  Set<Pessoa> listarPessoas() {
+    return _dao.listarPessoas();
+  }
+
+  Pessoa cadastrarPessoa(
+      String nome, String email, String telefone, int idade) {
+    return _dao.cadastrarPessoa(Pessoa.nova(nome, email, telefone, idade));
+  }
+
+  Pessoa atualizarPessoa(
+      int id, String nome, String email, String telefone, int idade) {
+    return _dao.atualizarPessoa(id, Pessoa(id, nome, email, telefone, idade));
+  }
+
+  bool removerPessoa(int id) {
+    return _dao.removerPessoa(id);
+  }
+}
